466b8037c72b4979a14332b06f12cac2ca5186cc
TedJenkler <Tedjenkler@gmail.com>
2023-11-22 05:55:03 -0200

    map logic

--- src/Component/Flags.js
+++ src/Component/Flags.js
@@ -1,32 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Flags = () => {
 
-    const [cName, setCName] = useState("")
-    const [cPopulation, setCPopulation] = useState("")
-    const [cRegion, setCRegion] = useState("")
-    const [cCapital, setCCapital] = useState("")
-    const [cFlag, setCFlag] = useState("")
+    const [cData, setCData] = useState([])
 
-    fetch('https://restcountries.com/v3.1/all')
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data[0].flags.svg)
-            setCName(data[0].name.official)
-            setCPopulation(data[0].population)
-            setCRegion(data[0].region)
-            setCCapital(data[0].capital[0])
-            setCFlag(data[0].flags.svg)
-          });
-    return (
-        <div className="card">
-            <img src={cFlag} alt={cName}></img>
-            <h1>{cName}</h1>
-            <p>Population: {cPopulation}</p>
-            <p>Region: {cRegion}</p>
-            <p>Capital: {cCapital}</p>
-        </div>
-    )
+    useEffect(() => {
+        const fetchData = async () => {
+          const response = await fetch('https://restcountries.com/v3.1/all')
+          const data = await response.json()
+          setCData(data)
+          console.log(data[0])
+        }
+
+        fetchData()
+      }, [])
+
+    return (<>
+        {cData.map((country) => {
+            return (
+                <div className="card">
+                    <img src={country.flags.svg} alt=""></img>
+                    <p>Capital: {country.capital}</p>
+                </div>
+            )
+        })}
+    </> )
 }
 
 export default Flags
