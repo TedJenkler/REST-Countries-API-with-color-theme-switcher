e8cc904237ab761562d205858933159f087ac44c
TedJenkler <Tedjenkler@gmail.com>
2023-11-22 07:07:09 -0200

    Flag render

--- src/Component/Flags.js
+++ src/Component/Flags.js
@@ -9,7 +9,7 @@ const Flags = () => {
           const response = await fetch('https://restcountries.com/v3.1/all')
           const data = await response.json()
           setCData(data)
-          console.log(data[0])
+          console.log(data)
         }
 
         fetchData()
@@ -20,6 +20,9 @@ const Flags = () => {
             return (
                 <div className="card">
                     <img src={country.flags.svg} alt=""></img>
+                    <h1>{country.name.official}</h1>
+                    <p>Population: {country.population}</p>
+                    <p>Region: {country.region}</p>
                     <p>Capital: {country.capital}</p>
                 </div>
             )
